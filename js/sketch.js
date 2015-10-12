80ff7f0bd8315725bc7feec60cc0694c267e9e00
Craig Protzel <craigprotzel@gmail.com>
2015-10-12 15:26:48 +0400

    Added files

---
+++ js/sketch.js
@@ -0,0 +1,25 @@
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(255);
+}
+
+function draw() {
+
+}
+
+
+function mousePressed(){
+
+}
+
+
+
+function Sand(){
+
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
